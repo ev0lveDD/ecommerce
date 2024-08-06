ddb9799ed8dc800e9f0fc97ccfee9591c155bf00
Jagiela <kjagiela93@gmail.com>
2024-08-06 15:52:51 +0200

    Further footer development, later it is needed to add main links

--- components/Footer/footer.js
+++ components/Footer/footer.js
@@ -1,32 +1,29 @@
 import Image from "next/image";
 
-import balmainLogo from "@/public/Balmain_logo.svg"
+import balmainLogo from "@/public/Balmain_logo.svg";
+import facebookIcon from "@/public/facebook-icon.svg";
+import instagramIcon from "@/public/instagram-icon.svg";
+import youtubeIcon from "@/public/youtube-icon.svg";
 
 export default function Footer(){
     return(
         <nav className="min-w-screen flex items-center justify-center bg-white">
-            <div className="flex justify-between items-center w-11/12 my-8">
+            <div className="flex flex-col gap-4 md:flex-row justify-between items-center w-11/12 my-8">
+                <p className="text-xs">Copyright © 2024 Balmain Inc. All rights reserved.</p>
                 <Image alt="Website Logo" width={65} height={50} src={balmainLogo} className="cursor-pointer z-50"/>
-                <div className="hidden md:flex">
-                    <ul className="flex justify-center items-center gap-6">
-                        <li className="text-sm cursor-pointer group transition duration-300">Link 1
-                            <span className="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-black"></span>
-                        </li>
-                        <li className="text-sm cursor-pointer group transition duration-300">Link 2
-                            <span className="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-black"></span>
-                        </li>
-                        <li className="text-sm cursor-pointer group transition duration-300">Link 3
-                            <span className="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-black"></span>
-                        </li>
-                        <li className="text-sm cursor-pointer group transition duration-300">Link 4
-                            <span className="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-black"></span>
-                        </li>
-                    </ul>
-                </div>
-                <div>
-                    <p>Facebook</p>
-                    <p>Instagram</p>
-                    <p>Youtube</p>
+
+                {/* Social Media */}
+                <div className="flex flex-col md:flex-row justify-between items-center gap-2">
+                    <p className="text-sm font-medium">Follow Us</p>
+                    <div className="flex items-center justify-center rounded-full border border-2 border-transparent hover:border-black cursor-pointer z-50">
+                        <Image alt="Facebook icon" width={20} height={20} src={facebookIcon} className="m-1.5"/>
+                    </div>
+                    <div className="flex items-center justify-center rounded-full border border-2 border-transparent hover:border-black cursor-pointer z-50">
+                        <Image alt="Instagram icon" width={20} height={20} src={instagramIcon} className="m-1.5"/>
+                    </div>
+                    <div className="flex items-center justify-center rounded-full border border-2 border-transparent hover:border-black cursor-pointer z-50">
+                        <Image alt="Youtube icon" width={20} height={20} src={youtubeIcon} className="m-1.5"/>
+                    </div> 
                 </div>
             </div>
         </nav>
--- components/Menu/ShoppingCart/shopping-cart.js
+++ components/Menu/ShoppingCart/shopping-cart.js
@@ -3,7 +3,7 @@ import cartIcon from "@/public/cart1.svg";
 
 export default function ShoppingCart({cartCounter}) {
     return(
-            <button className="relative flex items-center justify-center rounded-full border border-1 border-transparent hover:border-black z-50">
+            <button className="relative flex items-center justify-center rounded-full border border-2 border-transparent hover:border-black z-50">
                 <Image src={cartIcon} alt="Shopping Cart icon" height={30} width={27} className="m-1.5"/>
                 {cartCounter>0 ? 
                 <div className="absolute right-1.5 top-1 flex items-center justify-center rounded-full bg-rose-700 w-3 h-3">
