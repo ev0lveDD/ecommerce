ac58266d653414312b78ae5bc31f97334e7c54c4
Jagiela <kjagiela93@gmail.com>
2024-08-14 06:49:16 +0200

    Testing checbkox section approach

--- components/ProductList/FilterMenu/filter-menu.js
+++ components/ProductList/FilterMenu/filter-menu.js
@@ -1,13 +1,17 @@
+'use client';
+import { useState } from "react";
+
 import FilterDetailsDropdown from "./FilterDetailsDropdown/filter-details-dropdown";
 import ColorPickInput from "@/components/ProductDetails/ColorPickInput/color-pick-input";
 import FilterSizeButton from "./FilterSizeButton/filter-size-button";
 
 export default function FilterMenu({filterOpen, setFilterOpen, filterColorPicked, setFilterColorPicked, filterSizePicked, setFilterSizePicked}) {
 
+    const [isCheckboxChecked, setIsCheckboxchecked] = useState(false);
     return(
         <div className={`${filterOpen ? "w-full md:w-1/4" : "hidden"} min-h-screen flex flex-col items-center justify-start my-4`}>
 
-            <div className="w-4/5 h-screen flex flex-col items-center justify-start">
+            <div className="w-11/12 h-screen flex flex-col items-center justify-start">
                 <button onClick={() => setFilterOpen(!filterOpen)} className="!self-end m-4">
                     <p className="font-bold text-2xl">X</p>
                 </button>
@@ -39,16 +43,16 @@ export default function FilterMenu({filterOpen, setFilterOpen, filterColorPicked
                 </FilterDetailsDropdown>
 
                 <FilterDetailsDropdown dropdownId={"PriceFilterDropdown"} dropdownTitle={"BY PRICE"}>
-                    <div className="flex items-center my-4">
-                        <input id="price-filter-checkbox" type="checkbox" value="" className="hidden peer"></input>
-                        <label className="cursor-pointer flex-items-center justify center rounded-md border border-gray-200 p-2 font-bold text-gray-700 peer-checked:bg-black peer-checked:text-white peer-checked:border-black">
+                    <div className="flex items-center my-4 gap-2">
+                        <input id="price-filter-checkbox" type="checkbox" value="" className="peer z-10" checked={isCheckboxChecked} onChange={() => setIsCheckboxchecked(!isCheckboxChecked)}></input>
+                        <label className="z-0 flex items-center justify-start w-auto bg-white cursor-pointer hover:text-gray-800 peer-checked:font-bold" onClick={() => setIsCheckboxchecked(!isCheckboxChecked)}>
+                            <span className="text-sm font-medium text-gray-900">$19 - $30</span>
                         </label>
-                        <span className="ms-2 text-sm font-medium text-gray-900">$19 - $30</span>
                     </div>
                 </FilterDetailsDropdown>
 
                 <FilterDetailsDropdown dropdownId={"ApparelStyleFilterDropdown"} dropdownTitle={"APPAREL STYLE"}>
-                    <p className="font-black">TEST</p>
+                    <p className="font-medium">TEST</p>
                 </FilterDetailsDropdown>
             </div>
         </div>
