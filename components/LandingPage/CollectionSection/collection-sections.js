3173e7e109850b405e02ba4332f6344e03fc41d0
Jagiela <kjagiela93@gmail.com>
2024-08-20 17:05:34 +0200

    Further landing page development

---
+++ components/LandingPage/CollectionSection/collection-sections.js
@@ -0,0 +1,24 @@
+import Image from "next/image";
+
+import collectionBackground from "@/public/cbackground.png"
+
+export default function CollectionSection() {
+    return(
+
+        <div className="bg-collection-background bg-cover w-full h-[500px] flex items-center justify-center">
+            <div className="w-2/4 flex flex-col items-start justify-center gap-4">
+                <p className="hiden">X</p>
+            </div>
+            <div className="w-1/4 flex flex-col items-start justify-center gap-4">
+                <p className="text-white font-bold text-5xl">ZARA</p>
+                <p className="text-white text-sm">
+                    Lustrous Yet Understated. The New Evening Wear Collection Exclusively Offered At The
+                    Reopened Giorgio Armani Boutique in Los Angeles.
+                </p>
+                <button className="bg-white text-black py-3 px-3 rounded rounded-md text-sm hover:bg-gray-200 hober:text-gray-700">
+                    SEE COLLECTION
+                </button>
+            </div>
+        </div>
+    );
+}
--- components/LandingPage/landing-page.js
+++ components/LandingPage/landing-page.js
@@ -1,12 +1,17 @@
 import BrandsSection from "./BrandsSection/brands-section";
+import CollectionSection from "./CollectionSection/collection-sections";
 import PopularStylesSection from "./PopularStylesSection/popular-styles-section";
 
 export default function LandingPage() {
     return(
-        <div className="bg-white min-w-screen flex flex-col md:flex-row items-start justify-center my-4">
-            <div className="w-11/12 flex my-4 flex-col gap-8 items-center justify-center">
+        <div className="bg-white min-w-screen flex flex-col items-start justify-center my-4">
+            <div className="w-11/12 flex my-8 flex-col gap-8 items-center justify-center">
                 <BrandsSection />
                 <PopularStylesSection />
+            </div>
+            <CollectionSection />
+            <div className="w-11/12 flex my-8 flex-col gap-8 items-center justify-center">
+
             </div>
         </div>
     );
