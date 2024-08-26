7893d80a5bb9ca5c0ec35164251794482e4d4d56
Jagiela <kjagiela93@gmail.com>
2024-08-26 15:06:22 +0200

    Added landing page header carousel, working on carousel height

--- components/LandingPage/HeaderSection/header-section.js
+++ components/LandingPage/HeaderSection/header-section.js
@@ -1,6 +1,31 @@
+'use client';
+
+import Image from "next/image";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper/modules';
+
+
+import header1 from "@/public/header_images/header1.jpg";
+import header2 from "@/public/header_images/header2.jpg";
+import header3 from "@/public/header_images/header3.jpg";
+
 export default function HeaderSection() {
     return(
-        <div className="bg-collection-background bg-cover bg-[50px] w-full h-[512px] flex items-center justify-center">
+        <div className="w-full h-1/3 flex items-center justify-center z-10">
+            <Swiper 
+            modules={[Pagination]} 
+            pagination={true}
+            className="mySwiper max-h-full">
+                <SwiperSlide>
+                    <Image className="w-full h-[620px] object-cover object-top" alt="header image 1" src={header1}/>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <Image className="w-full h-[620px] object-cover object-top" alt="header image 2" src={header2}/>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <Image className="w-full h-[620px] object-cover object-top" alt="header image 3" src={header3}/>
+                </SwiperSlide>
+            </Swiper>
         </div>
     );
 }
