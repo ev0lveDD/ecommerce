a8566c057c9aadae3a7cc7a361ed87df0542d28e
Jagiela <kjagiela93@gmail.com>
2024-08-28 16:43:42 +0200

    Fixed header carousel height

--- components/LandingPage/HeaderSection/header-section.js
+++ components/LandingPage/HeaderSection/header-section.js
@@ -11,52 +11,52 @@ import 'swiper/css/pagination';
 
 export default function HeaderSection() {
     return(
-        <div className="w-full h-1/3 flex items-center justify-center z-10">
-            <Swiper 
-            autoplay={true}
-            modules={[Autoplay, Pagination]} 
-            pagination={{clickable: true}}
-            className="mySwiper">
-                <SwiperSlide>
-                    <div className="bg-header-background-1 bg-cover bg-top w-full h-[540px] flex items-center justify-center">
-                        <div className="bg-black/30 w-full h-full flex items-center justify-center md:justify-start">
-                            <div className="w-11/12 md:w-1/3 ml-0 md:ml-24 h-full flex flex-col items-start md:items-start justify-end md:justify-end gap-6">
-                                <h1 className="text-white font-bold text-3xl md:text-4xl ">BALMAIN SPRING COLLECTION</h1>
-                                <p className="text-white font-medium text-sm md:text-xl">FIND OUR OUR BEST SPRING COLLECTION. OFFERING OUR BEST QUALITY PRODUCT IN A BALMAIN SPRING COLLECTION.</p>
-                                <button className="bg-white text-black font-medium py-3 px-3 rounded rounded-md text-xs md:text-sm hover:bg-gray-200 hover:text-gray-700 mb-12">
-                                    BUY NOW
-                                </button>
+        <div className="w-full flex items-center justify-center z-10">
+                <Swiper 
+                autoplay={true}
+                modules={[Autoplay, Pagination]}  
+                pagination={{clickable: true}}
+                className="mySwiper">
+                    <SwiperSlide>
+                        <div className="bg-header-background-1 bg-cover bg-top bg-no-repeat w-full h-[540px] md:h-[560px] lg:h-[620px] flex items-center justify-center">
+                            <div className="bg-black/30 w-full h-full flex items-center justify-center md:justify-start">
+                                <div className="w-11/12 md:w-1/3 ml-0 md:ml-24 h-full flex flex-col items-start md:items-start justify-end md:justify-end gap-6">
+                                    <h1 className="text-white font-bold text-3xl md:text-4xl ">BALMAIN SPRING COLLECTION</h1>
+                                    <p className="text-white font-medium text-sm md:text-xl">FIND OUR OUR BEST SPRING COLLECTION. OFFERING OUR BEST QUALITY PRODUCT IN A BALMAIN SPRING COLLECTION.</p>
+                                    <button className="bg-white text-black font-medium py-3 px-3 rounded rounded-md text-xs md:text-sm hover:bg-gray-200 hover:text-gray-700 mb-12">
+                                        BUY NOW
+                                    </button>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div className="bg-header-background-2 bg-cover bg-top w-full h-[540px] flex items-center justify-center">
-                        <div className="bg-black/30 w-full h-full flex items-center justify-center md:justify-start">
-                            <div className="w-11/12 md:w-1/3 ml-0 md:ml-24 h-full flex flex-col items-start md:items-start justify-end md:justify-end gap-6">
-                                <h1 className="text-white font-bold text-3xl md:text-4xl ">COLLECTIONS</h1>
-                                <p className="text-white font-medium text-sm md:text-xl">YOU CAN EXPLORE AND SHOP MANY DIFFERENT COLLECTIONS FROM VARIOUS BRANDS.</p>
-                                <button className="bg-white mb-12 text-black font-medium py-3 px-3 rounded rounded-md text-xs md:text-sm hover:bg-gray-200 hover:text-gray-700 mb-12">
-                                    SHOP NOW
-                                </button>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div className="bg-header-background-2 bg-cover bg-top bg-no-repeat w-full h-[540px] md:h-[560px] lg:h-[620px] flex items-center justify-center">
+                            <div className="bg-black/30 w-full h-full flex items-center justify-center md:justify-start">
+                                <div className="w-11/12 md:w-1/3 ml-0 md:ml-24 h-full flex flex-col items-start md:items-start justify-end md:justify-end gap-6">
+                                    <h1 className="text-white font-bold text-3xl md:text-4xl ">COLLECTIONS</h1>
+                                    <p className="text-white font-medium text-sm md:text-xl">YOU CAN EXPLORE AND SHOP MANY DIFFERENT COLLECTIONS FROM VARIOUS BRANDS.</p>
+                                    <button className="bg-white mb-12 text-black font-medium py-3 px-3 rounded rounded-md text-xs md:text-sm hover:bg-gray-200 hover:text-gray-700 mb-12">
+                                        SHOP NOW
+                                    </button>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div className="bg-header-background-3 bg-cover bg-top w-full h-[540px] flex items-center justify-center">
-                        <div className="bg-black/30 w-full h-full flex items-center justify-center md:justify-start">
-                            <div className="w-11/12 md:w-1/3 ml-0 md:ml-24 h-full flex flex-col items-start md:items-start justify-end md:justify-end gap-6">
-                                <h1 className="text-white font-bold text-3xl md:text-4xl ">WOMENS FALL WINTER 2024 COLLECTION</h1>
-                                <p className="text-white font-medium text-sm md:text-xl">TAILORING EXPERTISE MEETING CRAFTSMANSHIP, HONORING JOY, LOVE & FREEDOM</p>
-                                <button className="bg-white mb-12 text-black font-medium py-3 px-3 rounded rounded-md text-xs md:text-sm hover:bg-gray-200 hover:text-gray-700">
-                                    DISCOVER
-                                </button>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div className="bg-header-background-3 bg-cover bg-top bg-no-repeat w-full h-[540px] md:h-[560px] lg:h-[620px] flex items-center justify-center">
+                            <div className="bg-black/30 w-full h-full flex items-center justify-center md:justify-start">
+                                <div className="w-11/12 md:w-1/3 ml-0 md:ml-24 h-full flex flex-col items-start md:items-start justify-end md:justify-end gap-6">
+                                    <h1 className="text-white font-bold text-3xl md:text-4xl ">WOMENS FALL WINTER 2024 COLLECTION</h1>
+                                    <p className="text-white font-medium text-sm md:text-xl">TAILORING EXPERTISE MEETING CRAFTSMANSHIP, HONORING JOY, LOVE & FREEDOM</p>
+                                    <button className="bg-white mb-12 text-black font-medium py-3 px-3 rounded rounded-md text-xs md:text-sm hover:bg-gray-200 hover:text-gray-700">
+                                        DISCOVER
+                                    </button>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </SwiperSlide>
-            </Swiper>
+                    </SwiperSlide>
+                </Swiper>
         </div>
     );
 }
--- components/LandingPage/landing-page.js
+++ components/LandingPage/landing-page.js
@@ -7,7 +7,7 @@ import PopularStylesSection from "./PopularStylesSection/popular-styles-section"
 
 export default function LandingPage() {
     return(
-        <div className="bg-white min-w-screen flex flex-col items-center justify-center"> {/* my-4 */}
+        <div className="bg-white min-w-screen min-h-screen flex flex-col items-center justify-center"> {/* my-4 */}
             <HeaderSection />
             <div className="w-full flex my-8 flex-col gap-8 items-center justify-center my-8">
                 <BrandsSection />
