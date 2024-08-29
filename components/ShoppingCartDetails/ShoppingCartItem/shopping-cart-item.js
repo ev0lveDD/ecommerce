f3cdc05253d45cebd7e0d7bc8cf6aa0172ea8b34
Jagiela <kjagiela93@gmail.com>
2024-08-29 16:39:04 +0200

    Adjusted image width&height added hook, to count item quantity

--- components/ShoppingCartDetails/ShoppingCartItem/shopping-cart-item.js
+++ components/ShoppingCartDetails/ShoppingCartItem/shopping-cart-item.js
@@ -1,31 +1,41 @@
+'use client';
+
 import Image from "next/image";
+import { useState } from "react";
 
-import image1 from "@/public/crewneck1.avif"
-import image2 from "@/public/mainLabZippedHoodie2.avif";
-import image3 from "@/public/perrebalmainhoodie1.avif";
 
-export default function ShoppingCartItem() {
+export default function ShoppingCartItem({cartItemImage, cartItemName, cartItemPrice, cartItemVariant, cartItemSize, cartItemColor}) {
+
+    const [itemQuantity, setItemQuantity] = useState(1);
+
+    const raiseQuantity = () => setItemQuantity(itemQuantity+1);
+    const lowerQuantity = () => {if(itemQuantity>1) {
+        setItemQuantity(itemQuantity-1);
+    } else {
+        null;
+    }};
+
     return(
-        <div className="w-full h-46 flex items-start justify-between border-b border-grey-lighter gap-4">
-            <Image className="h-38 w-36 my-4" alt="Item image" src={image2}/>
-            <div className="w-full min-h-48 flex flex-col items-start justify-between gap-4 my-4">
+        <div className="w-full h-48 flex items-start justify-between border-b border-grey-lighter gap-4">
+            <Image className="h-40 w-52 my-4 object-cover object-fill" alt="Item image" src={cartItemImage}/>
+            <div className="w-full min-h-40 flex flex-col items-start justify-between gap-4 my-4">
                 <div className="flex flex-col gap-3 w-full">
                     <div className="w-full h-full flex items-start justify-between">
-                        <p className="font-bold text-sm">BALMAIN LOGO-PRINT CREW-NECK SWEATSHIRT</p>
-                        <p className="font-bold text-sm">$595,00</p>
+                        <p className="font-bold text-sm">{cartItemName}</p>
+                        <p className="font-bold text-sm">${cartItemPrice},00</p>
                     </div>
                     <div className="flex flex-col items-start justify-center gap-1">
                         <div className="flex items-start justify-center gap-1">
-                            <p className="text-gray-400 font-bold text-xs">Variant: </p>
-                            <p className="text-gray-400 font-bold text-xs">XYZ</p>
+                            <p className="text-gray-400 font-semibold text-xs">Variant: </p>
+                            <p className="text-gray-400 font-semibold text-xs">{cartItemVariant}</p>
                         </div>
                         <div className="flex items-start justify-center gap-1">
-                            <p className="text-gray-400 font-bold text-xs">Size: </p>
-                            <p className="text-gray-400 font-bold text-xs">XL</p>
+                            <p className="text-gray-400 font-semibold text-xs">Size: </p>
+                            <p className="text-gray-400 font-semibold text-xs">{cartItemSize}</p>
                         </div>
                         <div className="flex items-start justify-center gap-1">
-                            <p className="text-gray-400 font-bold text-xs">Color:</p>
-                            <p className="text-gray-400 font-bold text-xs">White</p>
+                            <p className="text-gray-400 font-semibold text-xs">Color:</p>
+                            <p className="text-gray-400 font-semibold text-xs">{cartItemColor}</p>
                         </div>
                     </div>
                 </div>
@@ -34,9 +44,15 @@ export default function ShoppingCartItem() {
                         DELETE ITEM
                     </button>
                     <div className="flex items-center justify-center gap-2">
-                        <button className="bg-white text-black font-bold text-sm py-1 px-2.5 rounded rounded-full border border-1 border-black text-xs hover:text-gray-600 hover:border-gray-600">-</button>
-                        <p className="text-black font-bold text-md">1</p>
-                        <button className="bg-white text-black font-bold text-sm py-1 px-2 rounded rounded-full border border-1 border-black text-xs hover:text-gray-600 hover:border-gray-600">+</button>
+
+                        {itemQuantity===1 ? 
+                            <button disabled className="bg-white text-gray-300 font-black text-md py-1 px-2.5 rounded rounded-full border border-2 border-gray-300 text-xs hover:text-gray-500 hover:border-gray-500" onClick={lowerQuantity}>-</button> 
+                        :
+                            <button className="bg-white text-black font-black text-md py-1 px-2.5 rounded rounded-full border border-2 border-black text-xs hover:text-gray-600 hover:border-gray-600" onClick={lowerQuantity}>-</button>
+                        }
+
+                        <p className="text-black font-bold text-md">{itemQuantity}</p>
+                        <button className="bg-white text-black font-black text-md py-1 px-2 rounded rounded-full border border-2 border-black text-xs hover:text-gray-600 hover:border-gray-600" onClick={raiseQuantity}>+</button>
                     </div>
                 </div>
             </div>
--- components/ShoppingCartDetails/shopping-cart-details.js
+++ components/ShoppingCartDetails/shopping-cart-details.js
@@ -2,15 +2,19 @@ import ShoppingCartItem from "./ShoppingCartItem/shopping-cart-item";
 import SummarySection from "./SummarySection/summary-section";
 
 
+import image1 from "@/public/crewneck1.avif"
+import image2 from "@/public/mainLabZippedHoodie2.avif";
+import image3 from "@/public/perrebalmainhoodie1.avif";
+
 export default function ShoppingCartDetails() {
     return(
         <div className="bg-white min-w-screen flex items-center justify-center">
-            <div className="w-11/12 flex flex-col md:flex-row items-start justify-center gap-4 my-4">
-                <div className="w-full md:w-2/3 my-4 flex flex-col items-start justify-center gap-4">
+            <div className="w-11/12 flex flex-col md:flex-row items-start justify-center gap-16 my-4">
+                <div className="w-full md:w-2/3 my-4 flex flex-col items-center md:items-start justify-center gap-4">
                     <h1 className="text-3xl font-medium">YOUR CART</h1>
-                    <ShoppingCartItem />
-                    <ShoppingCartItem />
-                    <ShoppingCartItem />
+                    <ShoppingCartItem cartItemImage={image1} cartItemName={"BALMAIN LOGO-PRINT CREW-NECK SWEATSHIRT"} cartItemPrice={595} cartItemVariant={"SWEATSHIRT"} cartItemSize={"XL"} cartItemColor={"GREEN"}/>
+                    <ShoppingCartItem cartItemImage={image2} cartItemName={"BALMAIN HOODIE"} cartItemPrice={190} cartItemVariant={"HOODIE"} cartItemSize={"XL"} cartItemColor={"BLACK"}/>
+                    <ShoppingCartItem cartItemImage={image3} cartItemName={"BALMAIN LOGO-PRINT HOODIE"} cartItemPrice={300} cartItemVariant={"HOODIE"} cartItemSize={"XL"} cartItemColor={"NAVY"}/>
                 </div>
                 <SummarySection />
             </div>
